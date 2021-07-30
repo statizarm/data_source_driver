19b97aac284776d33480b25244c4e7ea7aa22070
Nasy <tncheetah758@gmail.com>
2021-07-31 01:11:05 +0300

    Added error handlers in sql translation

--- request/sqlbackend.go
+++ request/sqlbackend.go
@@ -1,13 +1,20 @@
 package request
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func (f Field) getSQLName() string {
-	if f.Child != nil {
-		return f.Name + "." + f.Child.getSQLName()
+func (f Field) getSQLName() (name string, err error) {
+	if f.Name == "" {
+		err = errors.New("in request/sqlbackend.go: getSQLName - Field name is empty or in an incorrect format")
+	} else if f.Child != nil {
+		subname, _ := f.Child.getSQLName()
+		name = f.Name + "." + subname
 	} else {
-		return f.Name
+		name = f.Name
 	}
+	return
 }
 
 // UnifiedRequestToSql method. Transforms a UnifiedRequest structure into a full SQL statement.
@@ -20,81 +27,183 @@ func translateToSQLSpecificForm (s *simplifiedRequest) (req string, err error) {
 		fields = "*"
 	} else {
 		for _, i := range s.Fields {
-			fields += i.getSQLName()
-			if i != s.Fields[len(s.Fields)-1] {
-				fields += ", "
+			if fld, e := i.getSQLName(); e != nil {
+				err = e
+				return "", err
+			} else {
+				fields += fld
+
+				if i != s.Fields[len(s.Fields)-1] {
+					fields += ", "
+				}
 			}
 		}
 	}
+	req += fields + " FROM "
+	if src := s.Source; src == "" {
+		err = errors.New("in request/sqlbackend.go: translateToSQLSpecificForm - Empty Source field")
+		return "", err
+	} else {
+		req += src + " WHERE "
+	}
 
-	req += fields + " FROM " + s.Source + " WHERE " + s.ReqExpr.translateToSQL()
-
+	if rexp, e := s.ReqExpr.translateToSQL(); e != nil{
+		err = e
+		return "", err
+	} else {
+		req += rexp
+	}
 	return
 }
 
 // translateToSQL method. Transforms a RequirementExpression into an SQL condition (after WHERE)
-func (expr *RequirementExpression) translateToSQL() string {
+func (expr *RequirementExpression) translateToSQL() (rexp string, err error) {
 	var temp string
 	switch expr.Type {
 	case "or":
-		temp = "(" + expr.OrAnd[0].translateToSQL()
-		for _, i := range expr.OrAnd[1:] {
-			temp += " OR " + i.translateToSQL()
+		if len(expr.OrAnd) < 2{
+			err = errors.New("in request/sqlbackend.go: ReqExpr.translateToSQL - Invalid number of expressions in OrAnd field")
+		} else {
+			if r, e := expr.OrAnd[0].translateToSQL(); e != nil {
+				err = e
+				return "", err
+			} else {
+				temp = "(" + r
+			}
+
+			for _, i := range expr.OrAnd[1:] {
+				if r, e := i.translateToSQL(); e != nil {
+					err = e
+					return "", err
+				} else {
+					temp += " OR " + r
+				}
+			}
+			return temp + ")", nil
 		}
-		return temp + ")"
 
 	case "and":
-		temp = "(" + expr.OrAnd[0].translateToSQL()
-		for _, i := range expr.OrAnd[1:] {
-			temp += " AND " + i.translateToSQL()
+		if len(expr.OrAnd) < 2{
+			err = errors.New("in request/sqlbackend.go: ReqExpr.translateToSQL - Invalid number of expressions in OrAnd field")
+		} else {
+			if r, e := expr.OrAnd[0].translateToSQL(); e != nil {
+				err = e
+				return "", err
+			} else {
+				temp = "(" + r
+			}
+
+			for _, i := range expr.OrAnd[1:] {
+				if r, e := i.translateToSQL(); e != nil {
+					err = e
+					return "", err
+				} else {
+					temp += " AND " + r
+				}
+			}
+			return temp + ")", nil
 		}
-		return temp + ")"
 
 	case "requirement":
-		return expr.Req.translateToSQL()
+		if r, e := expr.Req.translateToSQL(); e != nil {
+			err = e
+			return "", err
+		} else{
+			rexp = r
+		}
+
+	default:
+		err = errors.New("in request/sqlbackend.go: ReqExpr.translateToSQL - Wrong type of expression")
 	}
-	return ""
+	return
 }
 
 // translateToSQL method. Translates a requirement structure into an SQL requirement
-func (r *Requirement) translateToSQL() string {
-	var req string
-	req = "(" + r.Field.getSQLName()
+func (r *Requirement) translateToSQL() (req string, err error) {
+	if r, e := r.Field.getSQLName(); e != nil {
+		err = e
+		return "", err
+	} else {
+		req = "(" + r
+	}
+
 	switch r.Operator {
-	case "in": // Can be done with errors, but I'm not sure!
+	case "in":
 		values := r.Operand.Spec.([]interface{})
-		req += " IN (" + "'" + fmt.Sprint(values[0]) + "'"
-		for _, i := range values[1:] {
-			req += ", " + "'" + fmt.Sprint(i) + "'"
+		if len(values) < 1 {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty array of values")
+			return "", err
+		} else {
+			req += " IN (" + "'" + fmt.Sprint(values[0]) + "'"
+			for _, i := range values[1:] {
+				req += ", " + "'" + fmt.Sprint(i) + "'"
+			}
+			return req + "))", nil
 		}
-		return req + "))"
 
 	case "not in":
 		values := r.Operand.Spec.([]interface{})
-		req += " NOT IN (" + "'" + fmt.Sprint(values[0]) + "'"
-		for _, i := range values[1:] {
-			req += ", " + "'" + fmt.Sprint(i) + "'"
+		if len(values) < 1 {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty array of values")
+			return "", err
+		} else {
+			req += " NOT IN (" + "'" + fmt.Sprint(values[0]) + "'"
+			for _, i := range values[1:] {
+				req += ", " + "'" + fmt.Sprint(i) + "'"
+			}
+			return req + "))", nil
 		}
-		return req + "))"
 
 	case "ne":
-		return req + " <> '" + fmt.Sprint(r.Operand.Spec) + "')"
+		if spc := fmt.Sprint(r.Operand.Spec); spc == "" {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty value")
+			return "", err
+		} else {
+			return req + " <> '" + spc + "')", nil
+		}
 
 	case "eq":
-		return req + " = '" + fmt.Sprint(r.Operand.Spec) + "')"
+		if spc := fmt.Sprint(r.Operand.Spec); spc == "" {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty value")
+			return "", err
+		} else {
+			return req + " = '" + spc + "')", nil
+		}
 
 	case "ge":
-		return req + " >= " + fmt.Sprint(r.Operand.Spec) + ")"
+		if spc := fmt.Sprint(r.Operand.Spec); spc == "" {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty value")
+			return "", err
+		} else {
+			return req + " >= '" + spc + "')", nil
+		}
 
 	case "le":
-		return req + " <= " + fmt.Sprint(r.Operand.Spec) + ")"
+		if spc := fmt.Sprint(r.Operand.Spec); spc == "" {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty value")
+			return "", err
+		} else {
+			return req + " <= '" + spc + "')", nil
+		}
 
 	case "gt":
-		return req + " > " + fmt.Sprint(r.Operand.Spec) + ")"
+		if spc := fmt.Sprint(r.Operand.Spec); spc == "" {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty value")
+			return "", err
+		} else {
+			return req + " > '" + spc + "')", nil
+		}
 
 	case "lt":
-		return req + " < " + fmt.Sprint(r.Operand.Spec) + ")"
-	}
+		if spc := fmt.Sprint(r.Operand.Spec); spc == "" {
+			err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Empty value")
+			return "", err
+		} else {
+			return req + " < '" + spc + "')", nil
+		}
 
-	return ""
+	default:
+		err = errors.New("in request/sqlbackend.go: Requirement.translateToSQL - Wrong type of requirement operator")
+		return "", err
+	}
 }
